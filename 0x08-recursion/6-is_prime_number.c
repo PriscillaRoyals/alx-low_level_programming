b4f7ff6f3a022e13bf688ce832f227b11be4461c
PriscillaRoyals <ekhaguereprisca@gmail.com>
2022-11-02 09:34:06 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,36 +4,41 @@ int is_divisible(int num, int div);
 int is_prime_number(int n);
 
 /**
- * is_divisible - a function that returns 1 if the input integer is a prime number, otherwise return 0
- * @n: input number
- * @c: iterator.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
- */
-int is_prime(unsigned int n, unsigned int c)
-{
-	if (n % c == 0)
-	{
-		if (n == c)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + is_prime(n, c + 1));
-}
-/**
- * is_prime_number - Checks if a number is prime
- * @n: input number
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor
  *
- * Return: 1 if n is a prime number. 0 if n is not a prime number
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
  */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
+ */
+
 int is_prime_number(int n)
 {
-	if (n == 0)
+	int div = 2;
+
+	if (n <= 1)
 		return (0);
-	if (n < 0)
-		return (0);
-	if (n == 1)
-		return (0);
-	return (is_prime(n, 2));
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
 }
-	
